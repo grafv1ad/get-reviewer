02f9da16d355821a47433445332f148642a59472
Vladislav Petrov <grafv1ad@yandex.com>
2024-02-02 19:54:03 +0500

    add target _blank for link

--- src/components/Result/ResultItem.tsx
+++ src/components/Result/ResultItem.tsx
@@ -8,7 +8,7 @@ interface ResultItemProps {
 const ResultItem: React.FC<ResultItemProps> = ({data}) => {
     return (
         <li className="border p-1 transition-all hover:border-sky-800 hover:text-sky-800">
-            <a href={data.html_url} className="cursor-pointer flex gap-8 items-center">
+            <a href={data.html_url} target='_blank' className="cursor-pointer flex gap-8 items-center">
                 <img src={data.avatar_url} alt={data.login} style={{
                     width: '75px',
                     height: '75px',
